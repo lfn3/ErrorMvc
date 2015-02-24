ba6eb7af48d9605385ecb24bfa9905460e1b2db0
byAtlas <atlas@byatlas.com>
2015-02-24 22:10:37 +1300

    Initial commit - 4 ways to explode, and the default web.config

---
+++ ErrorMVC/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ErrorMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Exception()
+        {
+            throw new Exception("Exceptional!");
+        }
+
+        public ActionResult StackOverflow()
+        {
+            Overflow();
+
+            return View();
+        }
+
+        public static void Overflow()
+        {
+          Overflow();
+        }
+
+        public ActionResult TemplateException()
+        {
+            return View();
+        }
+
+        public ActionResult TemplateOverflow()
+        {
+            return View();
+        }
+    }
+}
---
+++ ErrorMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ErrorMVC.MvcApplication" Language="C#" %>
